adacba92cba70ae7702b9ce0e22324585fe6053b
jstephencorey <55117445+jstephencorey@users.noreply.github.com>
2022-02-18 20:06:16 -0700

    Added app.js

--- server/server/app.js
+++ server/server/app.js
@@ -1,34 +1,46 @@
 const express = require('express')
 const app = express()
-const port = 3000
+
+const port = process.env.PORT || 3000
+
+const { auth, requiresAuth } = require('express-openid-connect');
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    baseURL: 'http://localhost:3000',
+    secret: '7I6Blfjv0AmHkoWH2tZxinxgT1NrcGBOc7P0R8PBCSrGq_LHQ4DXdhtaHZExrwnd',
+    clientID: 'BJPIqF8mxThxnCv0bevxw97Dnf5SQVDG',
+    issuerBaseURL: 'https://dev-vkj75m74.us.auth0.com',
+};
+
+// // auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+app.listen(port, () => {
+    console.log(`Now listening on port ${port}`)
+})
+
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')
+        // res.send('Stuff')
+});
+
+
+app.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+});
 
 // app.get('/', (req, res) => {
 //     res.send('Hello World!')
 // })
 
-// app.get("/chocolate", function(req, res) {
-//     res.send("Mm chocolate :O");
-// });
+app.get("/chocolate", function(req, res) {
+    res.send("Mm chocolate :O");
+});
 
 // app.listen(port, () => {
 //     console.log(`Example app listening on port ${port}`)
 // })
-
-const { auth } = require('express-openid-connect');
-
-const config = {
-    authRequired: false,
-    auth0Logout: true,
-    secret: 'a very long, randomly-generated string stored in env',
-    baseURL: 'http://127.0.0.1:5501',
-    clientID: 'BJPIqF8mxThxnCv0bevxw97Dnf5SQVDG',
-    issuerBaseURL: 'https://dev-vkj75m74.us.auth0.com'
-};
-
-// auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config));
-
-// req.isAuthenticated is provided from the auth router
-app.get('/', (req, res) => {
-    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-});
